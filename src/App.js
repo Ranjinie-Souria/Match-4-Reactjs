c9a85c6a9c29ead1935fc5d28ca1f3249cb6169c
Ranjinie <ranjinie.souria@gmail.com>
2020-02-20 08:19:18 +0100

    Ajout de toutes les lignes possibles pour vérifier la victoire

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header"><h1>Puissance 4</h1><img src="logo.png" className="App-logo"/></header>
+                <header className="App-header"><h1>Match 4</h1><img src="logo.png" className="App-logo"/></header>
                 <div className="App-main"><Game/></div>
             </div>
         )
