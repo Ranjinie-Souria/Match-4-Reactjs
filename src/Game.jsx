5dd798d8fa7f4bf7b0b60efd59902d94360602db
Ranji <ranjinie.souria@gmail.com>
2026-04-13 16:22:46 +0200

    small design changes

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import Board from './Board';
-import Hoc from './HOC';
 
 function calculateWinner(squares) {
     let lines = [];
--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Match-4-Reactjs/"
 })
